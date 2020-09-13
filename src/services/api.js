1ee41ad167309277db101bddbda90189951a1ae8
Victor Lucas <victorlcmonteiro@gmail.com>
2020-09-13 09:46:28 -0300

    Bugfix - Mixed Content

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://newsapi.org/v2/everything?q=apple&from=2020-09-11&to=2020-09-11&sortBy=popularity&apiKey=05464b99b3b247f0aff76bc370e05ec0'
+  baseURL: 'https://newsapi.org/v2/everything?q=apple&from=2020-09-11&to=2020-09-11&sortBy=popularity&apiKey=05464b99b3b247f0aff76bc370e05ec0'
 })
 
 export default api;
